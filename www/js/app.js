18265fba57d29e33681a636c9d0f17d6e9369330
Orbachin Ujbuk <orbachinujubk@gmail.com>
2017-09-29 03:44:56 +0600

    Initial Commit (With half done!)

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','toaster'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -13,7 +13,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -25,18 +24,55 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
+  .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.welcome', {
+    url: '/welcome',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/welcome.html',
+        controller: 'WelcomeCtrl'
+      }
+    }
+  })
+  .state('app.entryperiod', {
+    url: '/entryperiod',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/entryperiod.html',
+        controller: 'EntryPeriodCtrl'
+      }
+    }
+  })
+  .state('app.periodcalendar', {
+    url: '/periodcalendar',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/periodcalendar.html',
+        controller: 'PeriodCalendarCtrl'
+      }
+    }
+  })
+  .state('app.periodlist', {
+    url: '/periodlist',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/periodlist.html',
+        controller: 'PeriodListCtrl'
+      }
+    }
+  })
+  .state('app.profile', {
+    url: '/profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/profile.html',
+        controller: 'ProfileCtrl'
       }
     }
   })
@@ -49,25 +85,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/welcome');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope,toaster,$ionicModal,$timeout,$http,$rootScope,$ionicPopup,$state,$ionicHistory,$ionicLoading) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -8,9 +8,16 @@ angular.module('starter.controllers', [])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-
+  // loads value from the loggedin session
   // Form data for the login modal
-  $scope.loginData = {};
+  $scope.user = {};
+
+  $scope.successToast = function(title, message){
+    toaster.pop('success', title, message);
+    // toaster.pop('error', title, message);
+    // toaster.pop('warning', title, message);
+    //toaster.pop('note', title, message);
+  };
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -29,28 +36,130 @@ angular.module('starter.controllers', [])
     $scope.modal.show();
   };
 
+  //logout function
+  $scope.logout= function(){
+    //delete all the sessions 
+    delete sessionStorage.loggedin_name;
+    delete sessionStorage.loggedin_id;
+    delete sessionStorage.loggedin_email;				
+    // remove the profile page backlink after logout.
+    $ionicHistory.nextViewOptions({
+      disableAnimate: true,
+      disableBack: true
+    });
+    
+    // After logout you will be redirected to the menu page,with no backlink
+    $ionicHistory.clearCache();
+    $rootScope.showLoginReg = true; 
+    $state.go('app.welcome', {}, {location: "replace", reload: true});
+    $scope.successToast('SUCCESS', 'Logged out successfully!');
+    //$ionicLoading.show({ template: 'Logged out successflly!', noBackdrop: true, duration: 2000});
+  };
+
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    console.log('Doing login', $scope.user);
+    str="http://localhost/angular_server1/login.php?email="+$scope.user.email+"&password="+$scope.user.password;
+    $http.get(str)
+    .success(function (response){   // if login request is Accepted 
+      // records is the 'server response array' variable name.
+      $scope.user_details = response.records;  // copy response values to user-details object.
+      //stores the data in the session. if the user is logged in, then there is no need to show login again.
+			sessionStorage.setItem('loggedin_name', $scope.user_details.name);
+			sessionStorage.setItem('loggedin_id', $scope.user_details.id );
+      sessionStorage.setItem('loggedin_email', $scope.user_details.email);
+				
+			// remove the instance of login page, when user moves to profile page.
+			// if you dont use it, you can get to login page, even if you are already logged in .
+			$ionicHistory.nextViewOptions({
+				disableAnimate: true,
+				disableBack: true
+      });
+
+			//redirect user        
+      console.log('Data received', $scope.user_details);
+      $rootScope.showLoginReg = false; 
+      $state.go('app.welcome', {}, {location: "replace", reload: true});
+      $scope.closeLogin();
+      $scope.successToast('SUCCESS', 'Logged in successfully!');
+      //$ionicLoading.show({ template: 'Logged in successflly!', noBackdrop: true, duration: 2000 });
+    }).error(function(error, status) {  						
+      //if login failed
+      $scope.status = status;
+      if(status <= 0) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Connection failed!',
+          template: 'Please check your internet connection!'
+        });
+      } else {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Login failed!',
+          template: 'Please check your credentials!'
+        });
+      }
+      
+    });
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
   };
+
+  $scope.loggedin_name = sessionStorage.getItem('loggedin_name');
+  $scope.loggedin_id = sessionStorage.getItem('loggedin_id');
+  $scope.loggedin_email = sessionStorage.getItem('loggedin_email');
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('PeriodListCtrl', function($scope,$http,$rootScope,$ionicHistory,$state,$ionicPopup) {  
+  // loads value from the loggedin session
+  $scope.loggedin_name= sessionStorage.getItem('loggedin_name');
+  $scope.loggedin_id= sessionStorage.getItem('loggedin_id');
+  $scope.loggedin_email= sessionStorage.getItem('loggedin_email');
+
+  //logout function
+  // $scope.logout= function(){
+	// 			//delete all the sessions 
+	// 			delete sessionStorage.loggedin_name;
+	// 			delete sessionStorage.loggedin_id;
+	// 			delete sessionStorage.loggedin_email;				
+	// 			// remove the profile page backlink after logout.
+	// 			$ionicHistory.nextViewOptions({
+	// 				disableAnimate: true,
+	// 				disableBack: true
+	// 			});
+				
+	// 			// After logout you will be redirected to the menu page,with no backlink
+	// 			$state.go('app.welcome', {}, {location: "replace", reload: true});
+	// 	};
+    if(!sessionStorage.getItem('loggedin_name')){   
+      //if not logged in
+      $state.go('app.welcome', {}, {location: "replace", reload: true});
+      var alertPopup = $ionicPopup.alert({
+        title: 'Error!',
+        template: 'Please login first!'
+      });
+    } else {
+      $http.get("http://localhost/angular_server1/customers_sql.php")
+      .then(function (response) {$scope.periods = response.data.records;});
+    }
+    
 })
 
+.controller('EntryPeriodCtrl', function($scope, $stateParams) {
+})
+.controller('PeriodCalendarCtrl', function($scope, $stateParams) {
+})
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+.controller('ProfileCtrl', function($scope, $stateParams) {
+})
+.controller('WelcomeCtrl', function($scope,$rootScope,$ionicHistory,$state) {  
+  // loads value from the loggedin session
+  $scope.loggedin_name= sessionStorage.getItem('loggedin_name');
+  $scope.loggedin_id= sessionStorage.getItem('loggedin_id');
+  $scope.loggedin_email= sessionStorage.getItem('loggedin_email');
+  if($scope.loggedin_id == null) {
+    $rootScope.showLoginReg = true;
+  } else {
+    $rootScope.showLoginReg = false; 
+  }
 });
